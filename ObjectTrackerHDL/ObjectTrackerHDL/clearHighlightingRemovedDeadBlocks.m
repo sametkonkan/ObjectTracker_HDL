989c3ee36cc62cfd4c36254d94e1d59c0af367f5
Samet Konkan <sametkonkan1@gmail.com>
2024-12-03 01:16:45 +0300

    HDL codes for Object Tracker Rescaled Version

---
+++ ObjectTrackerHDL/ObjectTrackerHDL/clearHighlightingRemovedDeadBlocks.m
@@ -0,0 +1,1 @@
+SLStudio.Utils.RemoveHighlighting(get_param('ObjectTrackerHDL','Handle'));
---
+++ ObjectTrackerHDL/ObjectTrackerHDL/highlightRemovedDeadBlocks.m
@@ -0,0 +1,6 @@
+cs.HiliteType = 'user1';
+cs.ForegroundColor = 'black';
+cs.BackgroundColor = 'red';
+set_param(0, 'HiliteAncestorsData', cs);
+hilite_system('ObjectTrackerHDL/ObjectTrackerHDL/Overlay/Demux','user1');
+hilite_system('ObjectTrackerHDL/ObjectTrackerHDL/Overlay/Mux','user1');
